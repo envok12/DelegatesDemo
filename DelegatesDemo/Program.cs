ba3b9b5a4dabb3b7e83a56c49a1b709541cbfd08
s12mitchell <s12mitchell@gmail.com>
2021-07-05 10:58:19 -0600

    Add project files.

---
+++ DelegatesDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DelegatesDemo
+{
+    delegate void DispStrDelegate(string param);
+    class Program
+    {
+        static void Capitalize(string text)
+        {
+            Console.WriteLine("Your input capatilized " + text.ToUpper());
+        }
+
+        static void LowerCased(string text)
+        {
+            Console.WriteLine("Your input lower cased " + text.ToLower());            
+        }
+
+        static void RunMyDelegate(DispStrDelegate del, string textParam)
+        {
+            del(textParam);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter some text: ");
+            String text = Console.ReadLine();
+
+            DispStrDelegate saying1 = Capitalize;
+            DispStrDelegate saying2 = LowerCased;
+            DispStrDelegate saying3 = Console.WriteLine;
+
+            saying1(text);
+            saying2(text);
+            saying3(text);
+
+            DispStrDelegate sayings = new DispStrDelegate(Capitalize);
+            sayings += new DispStrDelegate(LowerCased);
+            sayings += new DispStrDelegate(Console.WriteLine);
+
+            Console.WriteLine("Running multi cast directly: ");
+            sayings(text);
+
+            Console.WriteLine("Running by passing delegate to another method: ");
+
+            RunMyDelegate(sayings, text); 
+        }
+    }
+}
